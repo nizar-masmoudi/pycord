ef65701bb94fc90b56c20644923747c3f4707b01
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-01-23 21:47:43 +0100

    Add 2-Opt docstring

--- src/algorithms/localsearch.py
+++ src/algorithms/localsearch.py
@@ -24,6 +24,44 @@ class OPT:
           if costmat[self.path[i + 1], self.path[i]] + costmat[self.path[j + 1], self.path[j]] > costmat[self.path[j], self.path[i]] + costmat[self.path[j + 1], self.path[i + 1]]:
             self.swap(i, j)
             improved = True
+            
+class OPT:
+  @staticmethod
+  def swap(path: list, i: int, j: int) -> list:
+    path[i + 1], path[j] = path[j], path[i + 1]
+    path[i + 2:j] = list(reversed(path[i + 2:j]))
+    return path
+  
+  @staticmethod
+  def fit(costmat: Union[np.array, list]) -> Tuple[list, float]:
+    '''Runs the 2-Opt algorithm. This heuristic systematically swaps edges until finding a local minima.
+    
+    This method is a heuristic and returns only a local cost minima in exchange of execution speed.
+
+    # Parameters:
+    costmat (Union[np.array, list]): Cost matrix
+
+    # Returns:
+    Tuple[list, float]: Locally minimal path with its total cost (nodes are identified by their positions w.r. to the cost matrix)
+    
+    # Example:
+    >>> num_nodes = 20
+    >>> data = np.random.randint(0, 100, size = num_nodes*2).reshape(-1, 2)
+    >>> costmat = scipy.spatial.distance_matrix(data, data)
+    >>> path, cost = OPT.fit(costmat)
+    '''
+    path = list(range(len(costmat)))
+    path.append(path[0])
+    improved = True
+    while improved:
+      improved = False
+      for i in range(0, len(path) - 2):
+        for j in range(i + 2, len(path) - 1):
+          if costmat[path[i + 1], path[i]] + costmat[path[j + 1], path[j]] > costmat[path[j], path[i]] + costmat[path[j + 1], path[i + 1]]:
+            path = OPT.swap(path, i, j)
+            improved = True
+    min_cost = costmat[path, path[1:] + [0]]
+    return path, min_cost
       
 #####################################! Greedy Search !#####################################
 class Greedy:
--- src/test_data.py
+++ src/test_data.py
@@ -1,14 +1,11 @@
 import numpy as np
 from algorithms.exhaustive import BruteForce
-from algorithms.localsearch import Greedy
-from scipy.spatial import distance_matrix, KDTree
-import networkx as nx
-import matplotlib.pyplot as plt
-import numpy.ma as ma
+from algorithms.localsearch import Greedy, OPT
+from scipy.spatial import distance_matrix
 from utils import draw
 
 # np.random.seed(123)
-num_nodes = 10
+num_nodes = 20
 data = np.random.randint(0, 100, size = num_nodes*2).reshape(-1, 2)
 costmat = distance_matrix(data, data)
 
@@ -16,6 +13,6 @@ costmat = distance_matrix(data, data)
 
 # path, cost = Greedy.fit(costmat)
 
+path, cost = OPT.fit(costmat)
 
-
-# draw(data, path)
+draw(data, path)
